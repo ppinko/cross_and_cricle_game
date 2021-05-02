144b7f5e7bea5c1dbf5703839ed9f21abf6426ad
ppinko <p.pinkowicz@gmail.com>
2021-05-02 17:16:33 +0200

    IMPLEMENT - First working version of the game

--- cross_and_circle.go
+++ cross_and_circle.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // game variables
@@ -16,10 +18,10 @@ type dataMembers struct {
 
 func main() {
 
-	playGame()
 	data := dataMembers{
 		missing: 9,
 	}
+	data.playGame()
 	data.printBoard()
 	data.userInput()
 }
@@ -57,6 +59,8 @@ func (d *dataMembers) userInput() (int, int) {
 	// create an instance of buffered I/O
 	reader := bufio.NewReader(os.Stdin)
 	for {
+		fmt.Println("-----------------------------------------")
+		d.printBoard()
 		fmt.Println("-----------------------------------------")
 		fmt.Println("To quit the game press Q and press Enter.")
 		fmt.Println("Please choose a row to mark: ")
@@ -103,12 +107,48 @@ func (d *dataMembers) userInput() (int, int) {
 }
 
 // playGame starts new minesweeper game.
-func playGame() {
+func (d *dataMembers) playGame() {
 	fmt.Println("-----------------------------------------")
 	fmt.Println("WELCOME IN CROSS AND CIRCLE GAME")
 	fmt.Println("Please press an enter to start a game:")
 	fmt.Print("-> ")
 	reader := bufio.NewReader(os.Stdin)
+	reader.ReadString('\n')
+
+	// -1 - computer, 1 - player
+	turn := 1
+	for d.missing > 0 {
+		var row, col int
+		if turn == 1 {
+			row, col = d.userInput()
+		} else {
+			fmt.Println("Computer's turn!")
+			row, col = d.computerMove()
+		}
+
+		(*d).board[row][col] = turn
+		d.printBoard()
+		(*d).missing -= 1
+
+		if d.isWin(turn) {
+			d.printBoard()
+			if turn == 1 {
+				fmt.Println("Congratulations, you won!")
+			} else {
+				fmt.Println("You lost!")
+			}
+			fmt.Println("Please press an enter to leave the game:")
+			fmt.Print("-> ")
+			reader.ReadString('\n')
+		}
+
+		// change turn
+		turn *= -1
+	}
+
+	fmt.Println("A draw!")
+	fmt.Println("Please press an enter to leave the game:")
+	fmt.Print("-> ")
 	reader.ReadString('\n')
 }
 
@@ -118,4 +158,41 @@ func isValidField(d *dataMembers, row int, col int) bool {
 	} else {
 		return false
 	}
+}
+
+func (d *dataMembers) isWin(turn int) bool {
+	for i := 0; i < len(d.board); i++ {
+		if d.board[i][0] == turn && d.board[i][1] == turn && d.board[i][2] == turn {
+			return true
+		}
+		if d.board[0][i] == turn && d.board[1][i] == turn && d.board[2][i] == turn {
+			return true
+		}
+	}
+
+	if d.board[0][0] == turn && d.board[1][1] == turn && d.board[2][2] == turn {
+		return true
+	}
+	if d.board[0][2] == turn && d.board[1][1] == turn && d.board[2][0] == turn {
+		return true
+	}
+	return false
+}
+
+func (d *dataMembers) computerMove() (int, int) {
+	var emptyFields []int
+	for i := 0; i < len(d.board); i++ {
+		for j := 0; j < len(d.board[0]); j++ {
+			if d.board[i][j] == 0 {
+				emptyFields = append(emptyFields, 3*i+j)
+			}
+		}
+	}
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	newPosition := r.Intn(len(emptyFields))
+	val := emptyFields[newPosition]
+
+	return val / 3, val % 3
 }
